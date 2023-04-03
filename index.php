906f7620c011b6d5467be9c03f1880bfa1351094
MATHEUS ZANONI BITTENCOURT DE OLIVEIRA <oliveira.4942@aluno.pr.senac.br>
2023-04-03 08:45:20 -0300

    salvar

---
+++ index.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-aFq/bzH65dt+w6FI2ooMVUpc+21e0SRygnTpmBvdBgSdnuTN7QbdgL+OapgHtvPp" crossorigin="anonymous">
+        <link rel="stylesheet" href="meu.css">
+        <title>Zanoni</title>
+    </head>
+    <body>
+      <?php
+        echo "1";
+        echo"2";
+        echo"3";
+        echo"4";
+        echo"5";
+        echo "6";
+        echo"7";
+        echo"8";
+        echo"9";
+        echo"10";
+        for($numero=0; $numero<=10; $numero++){
+            echo $numero . "<br>";
+        }
+
+        
+        
+        
+      ?> 
+        
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha2/dist/js/bootstrap.bundle.min.js" integrity="sha384-qKXV1j0HvMUeCBQ+QVp7JcfGl760yU08IQ+GpUo5hlbpg51QRiuqHAJz8+BrxE/N" crossorigin="anonymous"></script>
+    </body>
+</html>
